21ac706f4cfeb32ac56f88267ce10f625bc643fe
harbo <757772438@qq.com>
2018-06-13 23:09:40 +0800

    应用lombok

--- src/main/java/com/nwuer/core/common/ResponseCode.java
+++ src/main/java/com/nwuer/core/common/ResponseCode.java
@@ -1,32 +1,22 @@
 package com.nwuer.core.common;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
 /**
  * @author vividzc
  * @date 2018/6/8 16:50
  */
 
+@Getter
+@AllArgsConstructor
 public enum ResponseCode {
-	SUCCESS(0,"SUCCCESS"),
-	ERROR(1,"ERROR"),
-	NEED_LOGIN(10,"NEED_LOGIN"),
-	ILLEGAL_ARGUMENT(2,"ILLEGAL_ARGUMENT");
-	
-	private final Integer code;
-	private final String desc;
-	
-	private ResponseCode(Integer code, String desc) {
-		this.code = code;
-		this.desc = desc;
-	}
+    SUCCESS(0, "SUCCESS"),
+    ERROR(1, "ERROR"),
+    NEED_LOGIN(10, "NEED_LOGIN"),
+    ILLEGAL_ARGUMENT(2, "ILLEGAL_ARGUMENT");
 
-	public Integer getCode() {
-		return code;
-	}
+    private final Integer code;
+    private final String desc;
 
-	public String getDesc() {
-		return desc;
-	}
-	
-	
-	
 }
