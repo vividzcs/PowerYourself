2d9dc84e3df3f646ae4ebf8a88a3166da96ca224
harbo <757772438@qq.com>
2018-06-13 23:21:13 +0800

    应用lombok

--- src/main/java/com/nwuer/core/common/ServerResponse.java
+++ src/main/java/com/nwuer/core/common/ServerResponse.java
@@ -1,5 +1,7 @@
 package com.nwuer.core.common;
 
+import lombok.*;
+
 import java.io.Serializable;
 
 /**
@@ -7,74 +9,61 @@ import java.io.Serializable;
  * @date 2018/6/8 16:50
  */
 
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@NoArgsConstructor
 public class ServerResponse<T> implements Serializable {
 	private Integer status;
 	private String msg;
 	private T data;
-	
-	private ServerResponse(Integer status, String msg, T data) {
-		this.status = status;
-		this.msg = msg;
-		this.data = data;
-	}
-	
+
 	private ServerResponse(Integer status, String msg) {
 		this.status = status;
 		this.msg = msg;
 	}
-	
+
 	private ServerResponse(Integer status, T data) {
 		this.status = status;
 		this.data = data;
 	}
-	
+
 	private ServerResponse(Integer status) {
 		this.status = status;
 	}
 
-	public Integer getStatus() {
-		return status;
-	}
 
-	public String getMsg() {
-		return msg;
-	}
-
-	public T getData() {
-		return data;
-	}
-	
 	public static <T>ServerResponse<T> createBySuccess(){
 		return new ServerResponse<>(ResponseCode.SUCCESS.getCode());
 	}
-	
+
 	public static <T>ServerResponse<T> createBySuccess(String message){
 		return new ServerResponse<>(ResponseCode.SUCCESS.getCode(),message);
 	}
-	
+
 	public static <T>ServerResponse<T> createBySuccess(String message,T data){
 		return new ServerResponse<>(ResponseCode.SUCCESS.getCode(),message,data);
 	}
-	
+
 	public static <T>ServerResponse<T> createBySuccess(T data){
 		return new ServerResponse<>(ResponseCode.SUCCESS.getCode(),data);
 	}
-	
+
 	public static <T>ServerResponse<T> createByError(){
 		return new ServerResponse<>(ResponseCode.ERROR.getCode(),ResponseCode.ERROR.getDesc());
-	}	
-	
+	}
+
 	public static <T>ServerResponse<T> createByErrorMessage(String message){
 		return new ServerResponse<>(ResponseCode.ERROR.getCode(),message);
 	}
-	
+
 	public static <T>ServerResponse<T> createByErrorMessage(Integer code,String message){
 		return new ServerResponse<>(code,message);
 	}
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 }
